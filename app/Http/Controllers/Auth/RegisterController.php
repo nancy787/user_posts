67f407b67ae022aac2f10b9ba47f9f93fefebf45
nancy787 <nancykumari2630@gmail.com>
2024-04-09 00:05:13 +0530

    updated role

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -63,11 +63,13 @@ class RegisterController extends Controller
      * @return \App\Models\User
      */
     protected function create(array $data)
-    {
+    {    
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'role' => $data['role']
         ]);
     }
+    
 }
